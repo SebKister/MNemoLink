9353a38209a2bf3e4f2b0a6d6237a683df81e972
Rodrigo Severo <rsev@pm.me>
2023-12-04 10:32:33 -0300

    Fixing tooltip presentation

--- lib/fileicon.dart
+++ lib/fileicon.dart
@@ -12,7 +12,7 @@ class FileIcon extends StatelessWidget {
     super.key,
     required this.extension,
     this.color = Colors.blue,
-    this.extensionColor=Colors.blue,
+    this.extensionColor = Colors.blue,
     this.size = 24.0,
     this.tooltip,
     this.onPressed,
@@ -21,38 +21,38 @@ class FileIcon extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      // Use Material to provide the necessary context for InkWell
-      color: Colors.transparent, // Ensure Material is transparent
-      child: InkWell(
-        onTap: onPressed,
-        borderRadius: BorderRadius.circular(
-            size), // Circular border radius for the ripple effect
-        child: ClipOval(
-          // Clip the content as a circle
-          child: SizedBox(
-            width: size * 1.6,
-            height: size * 1.6,
-            child: Stack(
-              alignment: Alignment.center,
-              children: <Widget>[
-                Icon(
-                  Icons.insert_drive_file,
-                  color: color,
-                  size: size,
-                ),
-                Positioned(
-                  bottom: 4,
-                  right: 4,
-                  child: Text(
-                    extension.toUpperCase(),
-                    style: TextStyle(
-                      fontSize: size * 0.5,
-                      fontWeight: FontWeight.w800,
-                      color: extensionColor,
+      color: Colors.transparent,
+      child: Tooltip(
+        message: tooltip ?? '',
+        child: InkWell(
+          onTap: onPressed,
+          borderRadius: BorderRadius.circular(size),
+          child: ClipOval(
+            child: SizedBox(
+              width: size * 1.6,
+              height: size * 1.6,
+              child: Stack(
+                alignment: Alignment.center,
+                children: <Widget>[
+                  Icon(
+                    Icons.insert_drive_file,
+                    color: color,
+                    size: size,
+                  ),
+                  Positioned(
+                    bottom: 4,
+                    right: 4,
+                    child: Text(
+                      extension.toUpperCase(),
+                      style: TextStyle(
+                        fontSize: size * 0.5,
+                        fontWeight: FontWeight.w800,
+                        color: extensionColor,
+                      ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
