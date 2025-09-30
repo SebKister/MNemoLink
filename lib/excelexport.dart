841d1a33e2ae63fd58cca0ebd8b3332ced175d94
Speleobrad <speleobrad@gmail.com>
2025-09-30 23:52:07 +0100

    excel export

--- lib/excelexport.dart
+++ lib/excelexport.dart
@@ -4,24 +4,10 @@ import 'package:intl/intl.dart';
 import 'package:excel/excel.dart';
 import 'models/models.dart';
 
+
 void writeHeaderOnSheet(Sheet sheet, int rowNumber) {
   var ls = [
-    "A",
-    "B",
-    "C",
-    "D",
-    "E",
-    "F",
-    "G",
-    "H",
-    "I",
-    "J",
-    "K",
-    "L",
-    "M",
-    "N",
-    "O",
-    "P"
+    "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S"
   ];
   int index = 0;
 
@@ -71,86 +57,84 @@ void writeHeaderOnSheet(Sheet sheet, int rowNumber) {
   cell.value = TextCellValue("Marker");
 
   cell = sheet.cell(CellIndex.indexByString("${ls[index++]}$rowNumber"));
+  cell.value = TextCellValue("Lidar Yaw");
+
+  cell = sheet.cell(CellIndex.indexByString("${ls[index++]}$rowNumber"));
+  cell.value = TextCellValue("Lidar Pitch");
+
+  cell = sheet.cell(CellIndex.indexByString("${ls[index++]}$rowNumber"));
+  cell.value = TextCellValue("Lidar Distance");
+
+  cell = sheet.cell(CellIndex.indexByString("${ls[index++]}$rowNumber"));
   cell.value = TextCellValue("Comments");
 }
 
-void writeRowOnSheet(Section section, Shot data, Sheet sheet, int rowNumber) {
+
+int writeRowOnSheet(Section section, Shot data, Sheet sheet, int startRowNumber) {
   var ls = [
-    "A",
-    "B",
-    "C",
-    "D",
-    "E",
-    "F",
-    "G",
-    "H",
-    "I",
-    "J",
-    "K",
-    "L",
-    "M",
-    "N",
-    "O",
-    "P"
+    "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S"
   ];
-  int index = 0;
 
   final cellStyleWithNumberFormatForNumber = CellStyle(
     numberFormat: NumFormat.standard_0,
   );
 
-  var cell = sheet.cell(CellIndex.indexByString("${ls[index++]}$rowNumber"));
+  int currentRow = startRowNumber;
+
+  // Write the main shot data row
+  int index = 0;
+  var cell = sheet.cell(CellIndex.indexByString("${ls[index++]}$currentRow"));
   cell.value = TextCellValue(data.typeShot.name);
 
-  cell = sheet.cell(CellIndex.indexByString("${ls[index++]}$rowNumber"));
+  cell = sheet.cell(CellIndex.indexByString("${ls[index++]}$currentRow"));
   cell.value = DoubleCellValue(data.getCalculatedLength());
   cell.cellStyle = cellStyleWithNumberFormatForNumber;
 
-  cell = sheet.cell(CellIndex.indexByString("${ls[index++]}$rowNumber"));
+  cell = sheet.cell(CellIndex.indexByString("${ls[index++]}$currentRow"));
   cell.value = DoubleCellValue(data.depthIn);
   cell.cellStyle = cellStyleWithNumberFormatForNumber;
 
-  cell = sheet.cell(CellIndex.indexByString("${ls[index++]}$rowNumber"));
+  cell = sheet.cell(CellIndex.indexByString("${ls[index++]}$currentRow"));
   cell.value = DoubleCellValue(data.depthOut);
   cell.cellStyle = cellStyleWithNumberFormatForNumber;
 
-  cell = sheet.cell(CellIndex.indexByString("${ls[index++]}$rowNumber"));
+  cell = sheet.cell(CellIndex.indexByString("${ls[index++]}$currentRow"));
   cell.value = DoubleCellValue(data.headingIn);
   cell.cellStyle = cellStyleWithNumberFormatForNumber;
 
-  cell = sheet.cell(CellIndex.indexByString("${ls[index++]}$rowNumber"));
+  cell = sheet.cell(CellIndex.indexByString("${ls[index++]}$currentRow"));
   cell.value = DoubleCellValue(data.headingOut);
   cell.cellStyle = cellStyleWithNumberFormatForNumber;
 
-  cell = sheet.cell(CellIndex.indexByString("${ls[index++]}$rowNumber"));
+  cell = sheet.cell(CellIndex.indexByString("${ls[index++]}$currentRow"));
   cell.value = DoubleCellValue(data.pitchIn);
   cell.cellStyle = cellStyleWithNumberFormatForNumber;
 
-  cell = sheet.cell(CellIndex.indexByString("${ls[index++]}$rowNumber"));
+  cell = sheet.cell(CellIndex.indexByString("${ls[index++]}$currentRow"));
   cell.value = DoubleCellValue(data.pitchOut);
   cell.cellStyle = cellStyleWithNumberFormatForNumber;
 
-  cell = sheet.cell(CellIndex.indexByString("${ls[index++]}$rowNumber"));
+  cell = sheet.cell(CellIndex.indexByString("${ls[index++]}$currentRow"));
   cell.value = DoubleCellValue(data.left);
   cell.cellStyle = cellStyleWithNumberFormatForNumber;
 
-  cell = sheet.cell(CellIndex.indexByString("${ls[index++]}$rowNumber"));
+  cell = sheet.cell(CellIndex.indexByString("${ls[index++]}$currentRow"));
   cell.value = DoubleCellValue(data.right);
   cell.cellStyle = cellStyleWithNumberFormatForNumber;
 
-  cell = sheet.cell(CellIndex.indexByString("${ls[index++]}$rowNumber"));
+  cell = sheet.cell(CellIndex.indexByString("${ls[index++]}$currentRow"));
   cell.value = DoubleCellValue(data.up);
   cell.cellStyle = cellStyleWithNumberFormatForNumber;
 
-  cell = sheet.cell(CellIndex.indexByString("${ls[index++]}$rowNumber"));
+  cell = sheet.cell(CellIndex.indexByString("${ls[index++]}$currentRow"));
   cell.value = DoubleCellValue(data.down);
   cell.cellStyle = cellStyleWithNumberFormatForNumber;
 
-  cell = sheet.cell(CellIndex.indexByString("${ls[index++]}$rowNumber"));
+  cell = sheet.cell(CellIndex.indexByString("${ls[index++]}$currentRow"));
   cell.value = DoubleCellValue(data.temperature);
   cell.cellStyle = cellStyleWithNumberFormatForNumber;
 
-  cell = sheet.cell(CellIndex.indexByString("${ls[index++]}$rowNumber"));
+  cell = sheet.cell(CellIndex.indexByString("${ls[index++]}$currentRow"));
   cell.value = TextCellValue(DateTime(
           section.dateSurvey.year,
           section.dateSurvey.month,
@@ -160,15 +144,49 @@ void writeRowOnSheet(Section section, Shot data, Sheet sheet, int rowNumber) {
           data.sec)
       .toIso8601String());
 
-  cell = sheet.cell(CellIndex.indexByString("${ls[index++]}$rowNumber"));
+  cell = sheet.cell(CellIndex.indexByString("${ls[index++]}$currentRow"));
   cell.value = IntCellValue(data.markerIndex);
   cell.cellStyle = cellStyleWithNumberFormatForNumber;
 
-  cell = sheet.cell(CellIndex.indexByString("${ls[index++]}$rowNumber"));
-  cell.value = TextCellValue(data.usesCalculatedLength() 
-      ? "Length calculated from depth change and inclination (original measurement was insufficient)" 
+  // Leave Lidar columns empty for main shot row
+  index += 3; // Skip Lidar Yaw, Pitch, Distance columns
+
+  cell = sheet.cell(CellIndex.indexByString("${ls[index++]}$currentRow"));
+  cell.value = TextCellValue(data.usesCalculatedLength()
+      ? "Length calculated from depth change and inclination (original measurement was insufficient)"
       : "");
+
+  currentRow++;
+
+  // Write Lidar data rows if available
+  if (data.hasLidarData() && data.lidarData != null) {
+    for (final lidarPoint in data.lidarData!.points) {
+      // Clear all columns for Lidar-only rows
+      index = 0;
+
+      // Skip all main shot data columns (15 columns)
+      index += 15;
+
+      // Write Lidar data
+      cell = sheet.cell(CellIndex.indexByString("${ls[index++]}$currentRow"));
+      cell.value = DoubleCellValue(lidarPoint.yaw);
+      cell.cellStyle = cellStyleWithNumberFormatForNumber;
+
+      cell = sheet.cell(CellIndex.indexByString("${ls[index++]}$currentRow"));
+      cell.value = DoubleCellValue(lidarPoint.pitch);
+      cell.cellStyle = cellStyleWithNumberFormatForNumber;
+
+      cell = sheet.cell(CellIndex.indexByString("${ls[index++]}$currentRow"));
+      cell.value = DoubleCellValue(lidarPoint.distance);
+      cell.cellStyle = cellStyleWithNumberFormatForNumber;
+
+      currentRow++;
+    }
+  }
+
+  return currentRow;
 }
+
 
 void writeTitleOnSheet(Sheet sheet, Section s, UnitType unitType) {
   var cell = sheet.cell(CellIndex.indexByString("A1"));
@@ -212,10 +230,17 @@ void exportAsExcel(SectionList sectionList, File file, UnitType unitType) {
 
     int rownum = 5;
 
+    // Check if this section has Lidar data and add a note if so
+    bool hasLidarData = element.getShots().any((shot) => shot.hasLidarData());
+    if (hasLidarData) {
+      var cell = sheet.cell(CellIndex.indexByString("A4"));
+      cell.value = TextCellValue("Survey includes Lidar data");
+    }
+
     writeHeaderOnSheet(sheet, rownum++);
-    element.getShots().forEach((data) {
-      writeRowOnSheet(element, data, sheet, rownum++);
-    });
+    for (final data in element.getShots()) {
+      rownum = writeRowOnSheet(element, data, sheet, rownum);
+    }
   }
 
   excel.delete("Sheet1");
